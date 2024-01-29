e39320c37dbd49bc92292b793705c30a9eadacae
ts7752 <113421474+ts7752@users.noreply.github.com>
2024-01-30 02:37:43 +0900

    Update HelloController.java

--- src/main/java/com/example/Lee/controller/HelloController.java
+++ src/main/java/com/example/Lee/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	@GetMapping("/test")
+	@RequestMapping("/test")
 	public String index() {
 		return "Hello jjongtae !";
 	}
