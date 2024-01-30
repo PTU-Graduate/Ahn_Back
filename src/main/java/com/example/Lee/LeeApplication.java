cd825c802849394ad0cd48578716ae6f0899b45f
ts7752 <ts7752@naver.com>
2024-01-30 16:59:54 +0900

    123

--- src/main/java/com/example/Lee/LeeApplication.java
+++ src/main/java/com/example/Lee/LeeApplication.java
@@ -2,9 +2,16 @@ package com.example.Lee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LeeApplication {
+public class LeeApplication extends SpringBootServletInitializer {
+
+	@Override // 구성 방법 재정의
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LeeApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeeApplication.class, args);
