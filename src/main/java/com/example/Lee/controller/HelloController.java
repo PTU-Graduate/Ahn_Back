b003af62b53c058237246ce1dea2a4aa3bf52760
ts7752 <ts7752@naver.com>
2024-04-15 05:36:57 +0900

    fix / 버그 해

--- src/main/java/com/example/Lee/controller/HelloController.java
+++ src/main/java/com/example/Lee/controller/HelloController.java
@@ -1,11 +1,7 @@
 package com.example.Lee.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Lee.service.DatabaseService;
@@ -20,19 +16,9 @@ public class HelloController {
 		this.databaseService = databaseService;
 	}
 
-	@PostMapping("/test")
+	@GetMapping("/test")
 	public String test() {
 		return "너는이종태다 키";
 	}
 
-	@PostMapping("/DbTest")
-	public List<Map<String, Object>> dbTest(@RequestBody Map<String, String> requestData) {
-		String endpoint = requestData.get("endpoint");
-		String data = requestData.get("data");
-		System.out.println("Endpoint: " + endpoint);
-		
-
-		// stu_info 테이블의 모든 데이터 반환
-		return databaseService.getAllDataFromTable("stu_info");
-	}
 }
