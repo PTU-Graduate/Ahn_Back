124dfcf995cc12e53fb37a61e014bc5f0455cb35
ts7752 <113421474+ts7752@users.noreply.github.com>
2024-04-15 16:58:36 +0900

    Fix / Server CORS 정책 수정 (#32)
    * feat / 주요 개발 (#26)
    
    DAO 기반 코드 개발
    Service 기반 코드 개발
    Controller 기반 코드 개발
    Model 기반 코드 개발
    글로벌 예외처리 기반 코드 개발
    라이브러리 추가
    PostMan Test 완료
    
    Co-authored-by: jeakyoung <85055229+jeakyoung@users.noreply.github.com>
    
    * FIX / Network Error Fix
    
    클라이언트 Axios Error 테스트
    
    * Dev (#29)
    
    * feat / 주요 개발
    
    DAO 기반 코드 개발
    Service 기반 코드 개발
    Controller 기반 코드 개발
    Model 기반 코드 개발
    글로벌 예외처리 기반 코드 개발
    라이브러리 추가
    PostMan Test 완료
    
    * FIX / Network Error Fix (#28)
    
    클라이언트 Axios Error 테스트
    
    ---------
    
    Co-authored-by: jeakyoung <85055229+jeakyoung@users.noreply.github.com>
    
    * fix / 클라이언트 Axios Error
    
    * Dev (#31)
    
    * feat / 주요 개발
    
    DAO 기반 코드 개발
    Service 기반 코드 개발
    Controller 기반 코드 개발
    Model 기반 코드 개발
    글로벌 예외처리 기반 코드 개발
    라이브러리 추가
    PostMan Test 완료
    
    * FIX / Network Error Fix (#28)
    
    클라이언트 Axios Error 테스트
    
    * Leejt (#30)
    
    * FIX / Network Error Fix
    
    클라이언트 Axios Error 테스트
    
    * fix / 클라이언트 Axios Error
    
    ---------
    
    Co-authored-by: jeakyoung <85055229+jeakyoung@users.noreply.github.com>
    
    * fix / 버그 해
    
    * fix / bug fix
    
    ---------
    
    Co-authored-by: jeakyoung <85055229+jeakyoung@users.noreply.github.com>

--- src/main/java/com/example/Lee/controller/HelloController.java
+++ src/main/java/com/example/Lee/controller/HelloController.java
@@ -1,16 +1,15 @@
 package com.example.Lee.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Lee.service.DatabaseService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET })
 public class HelloController {
 
 	private final DatabaseService databaseService;
@@ -20,19 +19,9 @@ public class HelloController {
 		this.databaseService = databaseService;
 	}
 
-	@PostMapping("/test")
+	@GetMapping("/test")
 	public String test() {
 		return "너는이종태다 키";
 	}
 
-	@PostMapping("/DbTest")
-	public List<Map<String, Object>> dbTest(@RequestBody Map<String, String> requestData) {
-		String endpoint = requestData.get("endpoint");
-		String data = requestData.get("data");
-		System.out.println("Endpoint: " + endpoint);
-		
-
-		// stu_info 테이블의 모든 데이터 반환
-		return databaseService.getAllDataFromTable("stu_info");
-	}
 }
