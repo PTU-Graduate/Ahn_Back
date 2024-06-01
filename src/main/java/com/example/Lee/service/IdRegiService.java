ebc31a5c3908f0352f7ed7d5d8ae0ff5a1d70c11
ts7752 <113421474+ts7752@users.noreply.github.com>
2024-06-01 17:34:32 +0900

    fix / 충돌 해결 (#77) (#78)
    * fix / 백엔드 코드 재수정 (#72) (#73)
    
    * fix/ 충돌해결 시도

--- src/main/java/com/example/Lee/service/IdRegiService.java
+++ src/main/java/com/example/Lee/service/IdRegiService.java
@@ -2,6 +2,8 @@ package com.example.Lee.service;
 
 import org.springframework.beans.factory.annotation.Autowired; // 스프링의 의존성 주입을 위한 어노테이션
 import org.springframework.stereotype.Service; // 스프링에서 서비스 계층의 컴포넌트를 정의하는 어노테이션
+import org.springframework.transaction.annotation.Propagation; //스프링에서 롤백의 범위를 지정하는 어노테이션
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Lee.dao.RegiRepositoryDao; // 회원 정보에 접근하기 위한 DAO
 import com.example.Lee.model.CommonResponseModel; // 클라이언트에 반환될 공통 응답 모델
@@ -16,14 +18,16 @@ public class IdRegiService {
 	public IdRegiService(RegiRepositoryDao regiRepository) {
 		this.regiRepository = regiRepository; // 생성자를 통해 주입받은 레포지토리 객체를 필드에 할당
 	}
-
+	
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public CommonResponseModel registerId(RegiModel regiData) {
 		// 사용자 ID가 이미 등록되어 있는지 확인
 		if (regiRepository.existsByMembId(regiData.getMembId())) {
 			return new CommonResponseModel("01"); // ID가 중복인 경우 응답 코드 "01" 반환
 		}
-		// 위 중복 검사를 통과하면 회원 정보를 데이터베이스에 저장
 		// ID 등록 성공 시 응답 코드 "00" 반환
 		return new CommonResponseModel("00");
 	}
+	
+
 }
--- src/main/java/com/example/Lee/service/MailRegiService.java
+++ src/main/java/com/example/Lee/service/MailRegiService.java
@@ -2,6 +2,8 @@ package com.example.Lee.service;
 
 import org.springframework.beans.factory.annotation.Autowired; // 스프링의 의존성 주입을 위한 어노테이션
 import org.springframework.stereotype.Service; // 스프링에서 서비스 계층의 컴포넌트를 정의하는 어노테이션
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Lee.dao.RegiRepositoryDao; // 회원 정보에 접근하기 위한 DAO
 import com.example.Lee.model.CommonResponseModel; // 클라이언트에 반환될 공통 응답 모델
@@ -16,15 +18,15 @@ public class MailRegiService {
 	public MailRegiService(RegiRepositoryDao regiRepository) {
 		this.regiRepository = regiRepository; // 생성자를 통해 주입받은 레포지토리 객체를 필드에 할당
 	}
-
+	
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	// 메일을 인자로 받아 중복여부를 확인하는 메서드
 	public CommonResponseModel mailRegister(RegiModel regiData) {
 		// 이메일이 이미 등록되어 있는지 확인
-		if (regiRepository.existsByStdNum(regiData.getStdNum())) {
+		if (regiRepository.existsByEmail(regiData.getEmail())) {
 			return new CommonResponseModel("03"); // 메일이 중복인 경우 응답 코드 "03" 반환
 		}
-		// 위 중복 검사를 통과하면 회원 정보를 데이터베이스에 저장
-		regiRepository.save(regiData);
-		// 회원 등록 성공 시 응답 코드 "00" 반환
+		// 이메일 등록 성공 시 응답 코드 "00" 반환
 		return new CommonResponseModel("00");
 	}
 }
